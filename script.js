43ee7f1842f8e9ea706fe97dedfff9f38450968c
João Pedro Garcia <eujoaopedrog@gmail.com>
2023-10-12 01:03:58 -0300

    Project - Previsão do tempo

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function colocarDadosNaTela(dados) {
     document.querySelector('.texto-previsao').innerHTML = dados.weather[0].description;
     document.querySelector('.umidade').innerHTML = 'Umidade: ' + dados.main.humidity + "%";
     document.querySelector('.img-previsao').src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
 }
 
 async function buscarCidade(cidade) {
-
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
     colocarDadosNaTela(dados)
 }
